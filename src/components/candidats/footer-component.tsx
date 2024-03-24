d3b1ee9a8ce1dd29f8db081ed9e1739a77fc6de5
mouridesadikh <mouride987@gmail.com>
2024-03-24 13:59:53 +0000

    graph des region

---
+++ src/components/candidats/footer-component.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import '../../styles/css/carte.css'
+export function FooterComponent() {
+    
+    return (
+         <div>
+               footer
+         </div>
+    )
+}
